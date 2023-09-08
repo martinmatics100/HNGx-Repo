2c27001b0802b90c1b782cef80dbd6417443ce4c
Martinmatics100 <martinchukwuemeka67@gmail.com>
2023-09-08 19:26:36 +0100

    commit all

---
+++ TaskOne/TaskOne/Controllers/InfoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using TaskOne.Implementation;
+
+namespace TaskOne.Controllers
+{
+    [Route("api/get[controller]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo(string slack_name, string track)
+        {
+            // Check if slack_name and track are provided and not empty
+            if (RequestValidator.IsNullOrEmpty(slack_name) || RequestValidator.IsNullOrEmpty(track))
+            {
+                var errorResponse = new
+                {
+                    error = "Invalid request. Both 'slack_name' and 'track' parameters are required."
+                };
+
+                return BadRequest(errorResponse);
+            }
+
+            // Check if the Slack name is valid using the RequestValidator
+            if (!RequestValidator.IsValidSlackName(slack_name, "martinmatics"))
+            {
+                var errorResponse = new
+                {
+                    error = "Invalid 'slack_name' parameter. Please provide a valid Slack name."
+                };
+
+                return BadRequest(errorResponse);
+            }
+
+            // Check if the track is valid using the RequestValidator
+            if (!RequestValidator.IsValidTrack(track))
+            {
+                var errorResponse = new
+                {
+                    error = "Invalid 'track' parameter. Please provide a valid track."
+                };
+
+                return BadRequest(errorResponse);
+            }
+
+            var jsonResponse = new
+            {
+                SlackName = "Martinmatics",
+                CurrentDay = DateTime.UtcNow.ToString("dddd", CultureInfo.InvariantCulture),
+                UtcTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                Track = "backend",
+                GithubFileUrl = "https://github.com/username/repo/blob/main/file_name.ext",
+                GithubRepoUrl = "https://github.com/martinmatics100/HNGx-Repo.git",
+                status_code = 200
+            };
+
+                return Ok(jsonResponse);
+        }
+    }
+}
---
+++ TaskOne/TaskOne/Implementation/RequestValidator.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskOne.Implementation
+{
+    public class RequestValidator
+    {
+        public static bool IsNullOrEmpty(string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsValidTrack(string track)
+        {
+            // Define a list of valid tracks (e.g., "backend" as specified in the requirements)
+            var validTracks = new List<string> { "backend" };
+
+            // Check if the provided track (case-insensitive) is in the list of valid tracks
+            return !string.IsNullOrEmpty(track) && validTracks.Contains(track.ToLowerInvariant());
+        }
+
+        public static bool IsValidSlackName(string slackName, string providedSlackName)
+        {
+            // Ensure the provided Slack name matches exactly (case-insensitive)
+            return !string.IsNullOrEmpty(slackName) &&
+                   slackName.Equals(providedSlackName, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
